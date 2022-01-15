23ec5ea1a4432489655e9196af75c0dc7c399553
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-15 12:14:04 -0800

    Add header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import { HomeOutlined, TrophyOutlined, LoginOutlined } from '@ant-design/icons';
+const { SubMenu } = Menu;
+
+const Header = () => {
+    return (
+        <Menu mode="horizontal">
+            <Menu.Item key="home" icon={<HomeOutlined />}>
+                Home
+            </Menu.Item>
+            <Menu.Item key="leaderboard" icon={<TrophyOutlined />}>
+                Leaderboard
+            </Menu.Item>
+            <Menu.Item key="login" icon={<LoginOutlined />}>
+                Login
+            </Menu.Item>
+        </Menu>
+    )
+}
+
+export default Header;
