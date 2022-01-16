ea40b7e6d203bc9dede01ec26063fec3fa849628
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-16 14:27:25 -0800

    Added fields for signup page

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,10 +1,145 @@
 import React from "react";
-import { Form } from "antd";
+import { Form, Input, Button } from "antd";
+import {
+    MailOutlined,
+    UserAddOutlined,
+    UserOutlined,
+    LockOutlined,
+} from "@ant-design/icons";
 
 const SignUp = () => {
+    const formItemLayout = {
+        labelCol: {
+            xs: { span: 24 },
+            sm: { span: 8 },
+        },
+        wrapperCol: {
+            xs: { span: 24 },
+            sm: { span: 16 },
+        },
+    };
+    const tailFormItemLayout = {
+        wrapperCol: {
+            xs: {
+                span: 24,
+                offset: 0,
+            },
+            sm: {
+                span: 16,
+                offset: 8,
+            },
+        },
+    };
+
     return (
         <div className="page">
-            <Form></Form>
+            <Form {...formItemLayout}>
+                <Form.Item
+                    name="email"
+                    label="E-mail"
+                    hasFeedback
+                    rules={[
+                        {
+                            type: "email",
+                            message: "The input is not valid E-mail!",
+                        },
+                        {
+                            required: true,
+                            message: "Please input your E-mail!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <MailOutlined className="site-form-item-icon" />
+                        }
+                        placeholder="user@email.com"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="username"
+                    label="Username"
+                    hasFeedback
+                    tooltip="How others will see you on the leaderboard."
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your username!",
+                            whitespace: true,
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <UserOutlined className="site-form-item-icon" />
+                        }
+                        placeholder="Username"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password
+                        prefix={
+                            <LockOutlined className="site-form-item-icon" />
+                        }
+                        type="password"
+                        placeholder="••••••••••"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="confirm"
+                    label="Confirm Password"
+                    dependencies={["password"]}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please confirm your password!",
+                        },
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                                if (
+                                    !value ||
+                                    getFieldValue("password") === value
+                                ) {
+                                    return Promise.resolve();
+                                }
+                                return Promise.reject(
+                                    new Error(
+                                        "The two passwords that you entered do not match!"
+                                    )
+                                );
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password
+                        prefix={
+                            <LockOutlined className="site-form-item-icon" />
+                        }
+                        type="password"
+                        placeholder="••••••••••"
+                    />
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        icon={<UserAddOutlined />}
+                    >
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     );
 };
