53baed04da0743d12d00f8579d3629804f769c26
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-16 15:20:47 -0800

    Create the login page

---
+++ client/src/pages/Login.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Input, Button } from "antd";
+import { LoginOutlined, UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const Login = () => {
+    const formItemLayout = {
+        labelCol: {
+            xs: { span: 24 },
+            sm: { span: 8 },
+        },
+        wrapperCol: {
+            xs: { span: 24 },
+            sm: { span: 16 },
+        },
+    };
+    const tailFormItemLayout = {
+        wrapperCol: {
+            xs: {
+                span: 24,
+                offset: 0,
+            },
+            sm: {
+                span: 16,
+                offset: 8,
+            },
+        },
+    };
+
+    return (
+        <div className="page">
+            <Form {...formItemLayout}>
+                <Form.Item
+                    name="username"
+                    label="Username/Email"
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your username or email!",
+                        },
+                    ]}
+                >
+                    <Input
+                        prefix={
+                            <UserOutlined className="site-form-item-icon" />
+                        }
+                        placeholder="Username/Email"
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your password!",
+                        },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password
+                        prefix={
+                            <LockOutlined className="site-form-item-icon" />
+                        }
+                        type="password"
+                        placeholder="••••••••••"
+                    />
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        icon={<LoginOutlined />}
+                    >
+                        Login
+                    </Button>
+                    &nbsp;Or <Link to="/signup">signup now!</Link>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+};
+
+export default Login;
