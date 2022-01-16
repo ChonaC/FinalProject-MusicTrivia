32d10d656607dcf41a39db40fdadb84aa58fe81b
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-16 15:01:18 -0800

    Add router and routes to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./App.css";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { Layout } from "antd";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
     return (
-        <Layout>
-            <Header />
-            <Layout
-                style={{
-                    padding: "0 50px",
-                    marginTop: 64,
-                    marginBottom: 64,
-                }}
-            >
-                <Home />
+        <Router>
+            <Layout style={{ height: "100vh" }}>
+                <Header />
+                <Layout
+                    style={{
+                        padding: "0 50px",
+                        marginTop: 64,
+                        marginBottom: 64,
+                    }}
+                >
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route exact path="/signup">
+                        <SignUp />
+                    </Route>
+                    <Route exact path="/login">
+                        <Login />
+                    </Route>
+                </Layout>
             </Layout>
-        </Layout>
+        </Router>
     );
 }
 
