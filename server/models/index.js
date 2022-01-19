376a5f6468a6373df7529b04e3d2369d6e165931
Daliya Benny <daliyaebenny@gmail.com>
2022-01-18 23:50:04 -0800

    Added  usermodel

--- server/models/index.js
+++ server/models/index.js
@@ -2,24 +2,6 @@ const User = require("./User");
 const Song = require("./Song");
 const Score = require("./Score");
 
-User.hasMany(Song, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE",
-});
-
-Song.belongsTo(User, {
-    foreignKey: "user_id",
-});
-
-User.hasMany(Score, {
-    foreignKey: "user_id",
-    onDelete: "CASCADE",
-});
-
-Score.belongsTo(User, {
-    foreignKey: "user_id",
-});
-
 module.exports = {
     Song,
     User,
--- server/models/user.js
+++ server/models/user.js
@@ -1,65 +1,53 @@
-const { Model, DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
-const sequelize = require("../config/connection");
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
-class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
-}
-
-User.init(
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  song: [
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [8],
-            },
-        },
+      type: Schema.Types.ObjectId,
+      ref: 'Song',
     },
+  ],
+  score: [
     {
-        hooks: {
-            beforeCreate: async (newUserData) => {
-                newUserData.password = await bcrypt.hash(
-                    newUserData.password,
-                    10
-                );
-                return newUserData;
-            },
-            beforeUpdate: async (updatedUserData) => {
-                updatedUserData.password = await bcrypt.hash(
-                    updatedUserData.password,
-                    10
-                );
-                return updatedUserData;
-            },
-        },
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "user",
-    }
-);
+      type: Schema.Types.ObjectId,
+      ref: 'Score',
+    },
+  ],
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
 
 module.exports = User;
