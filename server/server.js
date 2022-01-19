5af416f0f091616551c4581d9c1fc47db51c77fc
Daliya Benny <daliyaebenny@gmail.com>
2022-01-19 01:33:29 -0800

    Modified server.js to start using the data models

--- server/server.js
+++ server/server.js
@@ -1,45 +1,12 @@
 const express = require("express");
-const session = require("express-session");
-const routes = require("./controllers");
 const path = require("path");
-var cors = require("cors");
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const exphbs = require("express-handlebars");
-const helpers = require("./utils/helpers");
-
+const db = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
-const sess = {
-    secret: process.env.JAWSDB_SESSION_SECRET,
-    cookie: {
-        path: "/",
-        httpOnly: true,
-        secure: false,
-        maxAge: null,
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
-};
 
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, "/client/public")));
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-// Inform Express.js on which template engine to use
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () =>
-        console.log(`Server listening at http://localhost:${PORT}`)
-    );
-});
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    //   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
