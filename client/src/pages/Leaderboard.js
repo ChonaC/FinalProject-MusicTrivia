9b1ba17d49ff8003158bd2027fa7a9fda2d5dc12
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-20 00:38:48 -0800

    Update leaderboard

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -4,20 +4,36 @@ import { Table, Tag, Space } from "antd";
 const Leaderboard = () => {
     // ! For testing, remove for deployment
     const dataSource = [];
+    const randomDate = (start, end) => {
+        return new Date(
+            start.getTime() + Math.random() * (end.getTime() - start.getTime())
+        );
+    };
 
-    for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < 250; i++) {
+        let date = randomDate(new Date(2020, 0, 1), new Date());
         dataSource.push({
             key: i,
-            username: "ByteSizeError",
+            username: "ByteSizeErrorLong",
             score: Math.floor(Math.random() * 101),
-            date: "1/1/2022",
+            date:
+                (date.getMonth() > 8
+                    ? date.getMonth() + 1
+                    : "0" + (date.getMonth() + 1)) +
+                "/" +
+                (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+                "/" +
+                date.getFullYear(),
         });
     }
     // ! for testing
+    console.log(dataSource);
 
     const columns = [
         {
             title: "Rank",
+            key: "rank",
+            // width: 70,
             // * To get the index of the sorted array for the rank
             render: (item) => <>{dataSource.indexOf(item) + 1}</>,
         },
@@ -25,16 +41,23 @@ const Leaderboard = () => {
             title: "Username",
             dataIndex: "username",
             key: "username",
+            align: "center",
+            // ellipsis: true,
+        },
+
+        {
+            title: "Date",
+            dataIndex: "date",
+            key: "date",
+            align: "right",
+            // width: 93,
         },
         {
             title: "Score",
             dataIndex: "score",
             key: "score",
-        },
-        {
-            title: "Date",
-            dataIndex: "date",
-            key: "date",
+            align: "right",
+            // width: 80,
         },
     ];
 
