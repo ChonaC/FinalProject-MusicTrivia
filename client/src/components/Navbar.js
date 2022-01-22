76539b79dae8d148c96b3ae7697bc637ab8d400e
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-22 13:29:51 -0800

    Update navbar for when user is logged in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,29 @@
 import React from "react";
+import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
+
+import { useQuery } from "@apollo/client";
+import { QUERY_USER } from "../utils/queries";
+
 import { Menu } from "antd";
 import {
     HomeOutlined,
     TrophyOutlined,
     LoginOutlined,
+    LogoutOutlined,
     UserOutlined,
     PlusCircleOutlined,
 } from "@ant-design/icons";
 import logo from "../assets/images/music-trivia-logo.png";
 
 const Navbar = () => {
+    const { data } = useQuery(QUERY_USER);
+    let user;
+
+    if (data) {
+        user = data.user;
+    }
+
     return (
         <nav>
             <Menu mode="horizontal">
@@ -26,19 +39,32 @@ const Navbar = () => {
                 >
                     <Link to="/">Home</Link>
                 </Menu.Item>
-
-                <Menu.Item key="profile" icon={<UserOutlined />}>
-                    <Link to="/profile">Profile</Link>
-                </Menu.Item>
                 <Menu.Item key="leaderboard" icon={<TrophyOutlined />}>
                     <Link to="/leaderboard">Leaderboard</Link>
                 </Menu.Item>
-                <Menu.Item key="login" icon={<LoginOutlined />}>
-                    <Link to="/login">Log in</Link>
-                </Menu.Item>
-                <Menu.Item key="signup" icon={<PlusCircleOutlined />}>
-                    <Link to="/signup">Sign up</Link>
-                </Menu.Item>
+                {user ? (
+                    <>
+                        <Menu.Item key="profile" icon={<UserOutlined />}>
+                            <Link to="/profile">{user.username}</Link>
+                        </Menu.Item>
+                        <Menu.Item key="logout" icon={<LogoutOutlined />}>
+                            <Link to="/login" onClick={() => Auth.logout()}>
+                                Log Out
+                            </Link>
+                        </Menu.Item>
+                    </>
+                ) : null}
+
+                {user ? null : (
+                    <>
+                        <Menu.Item key="login" icon={<LoginOutlined />}>
+                            <Link to="/login">Log in</Link>
+                        </Menu.Item>
+                        <Menu.Item key="signup" icon={<PlusCircleOutlined />}>
+                            <Link to="/signup">Sign up</Link>
+                        </Menu.Item>
+                    </>
+                )}
             </Menu>
         </nav>
     );
