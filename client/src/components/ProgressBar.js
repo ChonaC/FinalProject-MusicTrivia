63e9b0f0b10d8ca69f77e6b1d34c9fcaed546da1
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-23 20:41:37 -0800

    Update progressbar to be live

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { Progress } from "antd";
 
-const ProgressBar = () => {
-    return <Progress percent={50} status="active" />;
+const ProgressBar = (props) => {
+    return (
+        <Progress
+            percent={Math.round((props.completed / props.length) * 100)}
+            status={
+                Math.round((props.completed / props.length) * 100) === 100
+                    ? "success"
+                    : "active"
+            }
+        />
+    );
 };
 
 export default ProgressBar;
