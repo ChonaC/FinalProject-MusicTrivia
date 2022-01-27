4b854b06d2347a37c1a983ee224af6b0bc697ce5
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-26 22:38:15 -0800

    Add searching on leaderboard

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { Table, Tag, Space } from "antd";
+import React, { useState } from "react";
+import { Table, Tag, Space, Input, Button } from "antd";
+import Highlighter from "react-highlight-words";
+import { SearchOutlined } from "@ant-design/icons";
 import { useQuery } from "@apollo/client";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 import { GET_SCORES } from "../utils/queries";
 
 const Leaderboard = () => {
-    // ! For testing, remove for deployment
     let { data } = useQuery(GET_SCORES);
     let dataSource = [];
 
@@ -15,6 +16,108 @@ const Leaderboard = () => {
         dataSource = needSort.sort((a, b) => (a.points < b.points ? 1 : -1));
         console.log(dataSource);
     }
+
+    const [searchText, setSearchText] = useState("");
+    const [searchedColumn, setSearchedColumn] = useState("");
+
+    let searchInput = "";
+
+    const getColumnSearchProps = (dataIndex, search) => ({
+        filterDropdown: ({
+            setSelectedKeys,
+            selectedKeys,
+            confirm,
+            clearFilters,
+        }) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    ref={(node) => {
+                        searchInput = node;
+                    }}
+                    placeholder={`Search ${search}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) =>
+                        setSelectedKeys(e.target.value ? [e.target.value] : [])
+                    }
+                    onPressEnter={() =>
+                        handleSearch(selectedKeys, confirm, dataIndex)
+                    }
+                    style={{ marginBottom: 8, display: "block" }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() =>
+                            handleSearch(selectedKeys, confirm, dataIndex)
+                        }
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => handleReset(clearFilters)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({ closeDropdown: false });
+
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{ color: filtered ? "#1890ff" : undefined }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                ? record[dataIndex]
+                      .toString()
+                      .toLowerCase()
+                      .includes(value.toLowerCase())
+                : "",
+        onFilterDropdownVisibleChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ""}
+                />
+            ) : (
+                text
+            ),
+    });
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText("");
+    };
 
     const columns = [
         {
@@ -28,13 +131,15 @@ const Leaderboard = () => {
             title: "Username",
             dataIndex: "username",
             key: "username",
-            align: "center",
+            // align: "center",
+            ...getColumnSearchProps("username", "Username"),
             // ellipsis: true,
         },
         {
             title: "Tags",
             key: "tags",
             dataIndex: "tags",
+            ...getColumnSearchProps("tags", "Artist"),
 
             render: (tags) => (
                 <>
@@ -65,6 +170,8 @@ const Leaderboard = () => {
             dataIndex: "date_created",
             key: "date",
             align: "right",
+            ...getColumnSearchProps("date_created", "Date"),
+
             // width: 93,
         },
         {
